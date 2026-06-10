da1a66407e698a48f8942c77c176232d2322b0d9
DarioM1992 <dario21992@gmail.com>
2026-06-10 17:48:19 +0200

    creato ciclo for per scrivere su html le card inserite

--- main.js
+++ main.js
@@ -7,10 +7,13 @@ const teamMembers = [
     {name: "Daniela Amet" ,role: "Analyst", img: "female3.png", email: "danielaamet@team.com"},
 ]
 
-const teamMemberCardTemple = ({ name, role, image, email }) => `
+const teamContainer = document.querySelector('.team-container');
+
+
+const teamMemberCardTemple = ({ name, role, img, email }) => `
     <div class="team-card">
         <div class="card-image">
-            <img src="assets/img/${image}" alt="${name}">
+            <img src="assets/img/${img}" alt="${name}">
         </div>
         <div class="card-text">
             <h3>${name}</h3>
@@ -19,3 +22,12 @@ const teamMemberCardTemple = ({ name, role, image, email }) => `
         </div>
     </div>
 `;
+
+let html = '' ;
+for (let i = 0 ; i < teamMembers.length; i++) {
+    const member = teamMembers[i];
+
+    html += teamMemberCardTemple(member);
+}
+
+teamContainer.innerHTML = html;
