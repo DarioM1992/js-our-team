329846adaa12501e7260bfb57d8850604206592d
DarioM1992 <dario21992@gmail.com>
2026-06-10 17:35:48 +0200

    creato array dei membri

---
+++ main.js
@@ -0,0 +1,8 @@
+const teamMembers = [
+    {name: "Marco Bianchi" ,role: "Designer", img: "male1.png", email: "marcobianchi@team.com"},
+    {name: "Laura Rossi" ,role: "Front-End Developer", img: "female1.png", email: "laurarossi@team.com"},
+    {name: "Giorgio Verdi" ,role: "Back-End Developer", img: "male2.png", email: "giorgioverdi@team.com"},
+    {name: "Marta Ipsum" ,role: "SEO Specialist", img: "female2.png", email: "martarossi@team.com"},
+    {name: "Roberto Lorem" ,role: "SEO Specialist", img: "male3.png", email: "robertolorem@team.com"},
+    {name: "Daniela Amet" ,role: "Analyst", img: "female3.png", email: "danielaamet@team.com"},
+]
